654b4f4e23eaaf67aaa1b276cbc3f3a401d4016e
Rodrigo Catão Araujo <rodrigo.araujo13@gmail.com>
2015-10-19 21:15:59 -0200

    Initial

---
+++ src/Neuron.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Neuron {
+    private ArrayList<Synapse> synapses = new ArrayList<>();
+    private String name; /*Just for test purpose*/
+    
+    
+}
